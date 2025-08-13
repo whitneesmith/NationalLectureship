25679de0029ce6e64ea861db30e58ef4c3902038
whitneesmith <whitsmit919@gmail.com>
2025-08-13 01:40:48 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from './pages/Home';
 import Register from './pages/Register';
-import RegisterForm from './pages/RegisterForm';
-import Hotel from './pages/Hotel';
-import Vendors from './pages/Vendors';
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
-        <Layout>
+        <Header />
+        <main>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
-            <Route path="/register-form" element={<RegisterForm />} />
-            <Route path="/hotel" element={<Hotel />} />
-            <Route path="/vendors" element={<Vendors />} />
           </Routes>
-        </Layout>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
